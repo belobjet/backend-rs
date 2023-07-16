04bfde85d392ea1a152b43ebe5819065b6e673ec
Maxime Cattet <maximecattet@gmx.com>
2023-07-16 14:01:53 -0600

    feat: Add datetime

--- src/schema.rs
+++ src/schema.rs
@@ -1,8 +1,11 @@
+use chrono::{DateTime, Utc};
 use juniper::{EmptyMutation, EmptySubscription, FieldResult, GraphQLObject, RootNode};
 
 #[derive(GraphQLObject)]
 pub struct Bounty {
     id: String,
+    created_at: DateTime<Utc>,
+    updated_at: DateTime<Utc>,
     title: String,
     content: String,
     nb_comments: i32,
@@ -16,12 +19,16 @@ impl QueryRoot {
         Ok(vec![
             Bounty {
                 id: "1".to_owned(),
+                created_at: Utc::now(),
+                updated_at: Utc::now(),
                 title: "Bounty 1".to_owned(),
                 content: "Bounty 1 content".to_owned(),
                 nb_comments: 0,
             },
             Bounty {
                 id: "2".to_owned(),
+                created_at: Utc::now(),
+                updated_at: Utc::now(),
                 title: "Bounty 2".to_owned(),
                 content: "Bounty 2 content".to_owned(),
                 nb_comments: 0,
